00bbcd5936b26f6da57a55cde35c7b642333fd49
Saurabh-Taylor <tailorsaurabh12@gmail.com>
2024-04-21 16:46:30 +0530

    added not found page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@ import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import Home from './Pages/Home'
 import AboutUs from './Pages/AboutUs'
+import NotFound from './Pages/NotFound'
+
+
 
 
 
@@ -17,6 +20,10 @@ function App() {
       <Routes>
         <Route path='/' element={<Home/>} ></Route>
         <Route path='/about' element={<AboutUs/>} ></Route>
+
+
+
+        <Route path='*' element={<NotFound/>} ></Route>
       </Routes>
       
     </>
